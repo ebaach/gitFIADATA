c9e3e39ff40e171befd64e2665acedd2d8b73bf1
Elizabeth Baach <baach.elizabeth@gmail.com>
2021-02-14 14:36:52 -0600

    2/14 merging successful and successful spread function so far

--- FIA data/rFIA remote.R
+++ FIA data/rFIA remote.R
@@ -1,3 +1,7 @@
+
+# set up ------------------------------------------------------------------
+
+
 #first lets load our packages
 library(rFIA)
 library(dplyr)
@@ -6,14 +10,60 @@ library(tidyverse)
 #will this pathway work better?
 getFIA(c('MS'), dir = 'C:/Users/elbaa/OneDrive/Desktop/gitMSDATA', load = FALSE)
 
+#use this to load--- last code is for downloading if this doesnt work
 MS <- readFIA('C:/Users/elbaa/OneDrive/Desktop/gitMSDATA', inMemory = FALSE)
 
 
-#from tips on working with big data
+# rFIA functions(loading) -------------------------------------------------
+
+
 #must first define db at fia 
 biomass(db = MS)
 
-# ## By species
+#grouping by plt origin code
+originbio<- biomass(MS, byPlot = TRUE, grpBy = c("STDORGCD")) %>% filter(STDORGCD ==0)
+origindiv<- diversity(MS, byPlot = TRUE, grpBy = "STDORGCD")%>% filter(STDORGCD ==0)
+originvit<- vitalRates(MS, byPlot = TRUE, grpBy = "STDORGCD")%>% filter(STDORGCD ==0)
+
+
+# merging rFIA functions --------------------------------------------------
+
+
+#adding columns I want from div and vit to bio under new name
+adddata<- originbio
+#from the other rFIA tables
+adddata$S <- origindiv$S[match(adddata$pltID, origindiv$pltID)]
+adddata$H<- origindiv$H[match(adddata$pltID, origindiv$pltID)]
+adddata$BIO_GROW<- originvit$BIO_GROW[match(adddata$pltID,originvit$pltID)]
+adddata$BIO_GROW_AC<- originvit$BIO_GROW_AC[match(adddata$pltID, originvit$pltID)]
+#now from the other 'main' tables
+
+# loading main tables and merging them to main ----------------------------
+
+
+#now to read the 'main' tables we will end up needing
+TREE <- readFIA(dir = 'C:/Users/elbaa/OneDrive/Desktop/gitMSDATA', tables = 'TREE', inMemory = TRUE)
+COND <- readFIA(dir = 'C:/Users/elbaa/OneDrive/Desktop/gitMSDATA', tables = 'COND', inMemory = TRUE)
+#heres from tree
+adddata$SPCD<- TREE$TREE$SPCD[match(adddata$PLT_CN, TREE$TREE$PLT_CN)]
+adddata$COUNTYCD<- TREE$TREE$COUNTYCD[match(adddata$PLT_CN, TREE$TREE$PLT_CN)]
+adddata$UNITCD <- TREE$TREE$UNITCD[match(adddata$PLT_CN, TREE$TREE$PLT_CN)]
+adddata$DRYBIO_AG <- TREE$TREE$DRYBIO_AG[match(adddata$PLT_CN, TREE$TREE$PLT_CN)]
+#heres from cond
+adddata$FORTYPCD <- COND$COND$FORTYPCD[match(adddata$PLT_CN, COND$COND$PLT_CN)]
+
+#ok lets try the spread function
+newtest<- adddata %>% group_by(pltID,YEAR) %>% summarise(changebiomass= sum(DRYBIO_AG))
+newtest<- newtest %>% filter(biomass>0)
+spdtest<- spread(newtest, YEAR, changebiomass, fill = NA)
+#this part below isnt working
+# spdtest$mct <- rowSums(is.na(spdtest))
+# spdtest<- spdtest %>% filter(mct<10)
+
+
+
+# #PAST work
+#By species
 # biosp<-biomass(MS, bySpecies = TRUE)
 
 # #by plot
@@ -24,34 +74,27 @@ biomass(db = MS)
 # div_plt<- diversity(MS, byPlot = TRUE)
 # n_distinct(div_plt$H)
 
-
-#grouping by plt origin code
-originbio<- biomass(MS, byPlot = TRUE, grpBy = c("STDORGCD")) %>% filter(STDORGCD ==0)
-origindiv<- diversity(MS, byPlot = TRUE, grpBy = "STDORGCD")%>% filter(STDORGCD ==0)
-originvit<- vitalRates(MS, byPlot = TRUE, grpBy = "STDORGCD")%>% filter(STDORGCD ==0)
-# 
 # #growth rates
-# vits<- vitalRates(MS, byPlot = TRUE)
-
-n_distinct(vits$BIO_GROW_AC)
-#merge bioplt and divplt
-div_bio<- div_plt %>% left_join(bioplt, by =c("pltID"="pltID","YEAR"="YEAR","PLOT_STATUS"="PLOT_STATUS","PLOT_STATUS_CD"="PLOT_STATUS_CD","nStems"="nStems","PLT_CN"="PLT_CN"))
-
-div_bio<- div_bio %>% filter(PLOT_STATUS=="Forest")
-n_distinct(bioplt$S)
-
-bioplt$H<- div_plt$H[match(bioplt$pltID,div_plt$pltID)]
-
-bioplt$S <- div_plt$S[match(bioplt$pltID, div_plt$pltID)]
-bioplt$STDORGCD<- originbio$STDORGCD[match(bioplt$pltID, originbio$pltID)]
-
-divbiovital<- bioplt %>% left_join(vits, by = c("pltID"="pltID"))
-
-n_distinct(divbiovital$BIO_ACRE)
-
-## Alternative estimators (annual panels)
-yrbio<-biomass(MS, method = 'ANNUAL')
-#plot of biomass/acre/yr
-ggplot(yrbio, aes(x= yrbio$YEAR, y= yrbio$BIO_ACRE))+ geom_line()
+# # vits<- vitalRates(MS, byPlot = TRUE)
+# n_distinct(vits$BIO_GROW_AC)
+# 
+# #merge bioplt and divplt
+# div_bio<- div_plt %>% left_join(bioplt, by =c("pltID"="pltID","YEAR"="YEAR","PLOT_STATUS"="PLOT_STATUS","PLOT_STATUS_CD"="PLOT_STATUS_CD","nStems"="nStems","PLT_CN"="PLT_CN"))
+# 
+# div_bio<- div_bio %>% filter(PLOT_STATUS=="Forest")
+# n_distinct(bioplt$S)
+# 
+# bioplt$H<- div_plt$H[match(bioplt$pltID,div_plt$pltID)]
+# 
+# bioplt$S <- div_plt$S[match(bioplt$pltID, div_plt$pltID)]
+# bioplt$STDORGCD<- originbio$STDORGCD[match(bioplt$pltID, originbio$pltID)]
+# 
+# divbiovital<- bioplt %>% left_join(vits, by = c("pltID"="pltID"))
+# 
+# n_distinct(divbiovital$BIO_ACRE)
+# 
+# ## Alternative estimators (annual panels)
+# yrbio<-biomass(MS, method = 'ANNUAL')
+# #plot of biomass/acre/yr
 
        
