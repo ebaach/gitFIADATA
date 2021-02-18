ef37f5e2b96c8c7e234da00a2482eeb3bafdd92f
Elizabeth Baach <baach.elizabeth@gmail.com>
2021-02-17 23:07:35 -0600

    2/17 graphs for poster init

--- FIA data/rFIA remote.R
+++ FIA data/rFIA remote.R
@@ -6,9 +6,9 @@
 library(rFIA)
 library(dplyr)
 library(tidyverse)
-
-#will this pathway work better?
-getFIA(c('MS'), dir = 'C:/Users/elbaa/OneDrive/Desktop/gitMSDATA', load = FALSE)
+# 
+# for downloading
+# getFIA(c('MS'), dir = 'C:/Users/elbaa/OneDrive/Desktop/gitMSDATA', load = FALSE)
 
 #use this to load--- last code is for downloading if this doesnt work
 MS <- readFIA('C:/Users/elbaa/OneDrive/Desktop/gitMSDATA', inMemory = FALSE)
@@ -57,9 +57,37 @@ newtest<- adddata %>% group_by(pltID,YEAR) %>% summarise(biomass= sum(DRYBIO_AG)
 newtest<- newtest %>% filter(biomass>0)
 spdtest<- spread(newtest, YEAR, biomass, fill = NA)
 #this part below isnt working
-# spdtest$mct <- rowSums(is.na(spdtest))
-# spdtest<- spdtest %>% filter(mct<10)
+spdtest$mct <- rowSums(is.na(spdtest))
+spdtest<- spdtest %>% filter(mct<10)
+#testing
+vittest<- adddata %>% filter(pltID=='3_28_101_51')
+vittest$BIO_GROW_AC
 
+#lets do one for diversity
+divtest<- adddata %>% group_by(pltID,UNITCD) %>% summarise(speciesrichness= S)
+divtest<- divtest %>% filter(speciesrichness>0)
+shantest<- adddata %>% group_by(pltID, UNITCD) %>% summarise(Shannon=H)
+shantest<- shantest %>% filter(Shannon>0)
+
+#species richness (S)
+#divided by unitcd
+ggplot(divtest, aes(x= speciesrichness))+geom_histogram(binwidth = .5)+ labs(title = "Distribution of Plot Tree Species Richness", y="Number of Plots",x="Species Richness")+facet_grid(UNITCD~.,labeller = labeller(UNITCD=supp.labs))+theme(plot.title = element_text(hjust = 0.5))
+supp.labs <- c("Delta", "North","Central","South","Southwest")
+names(supp.labs)<- c("1","2","3","4","5")
+
+#overall
+ggplot(divtest, aes(x= speciesrichness))+geom_histogram(binwidth = .5)+ labs(title = "Distribution of Plot Tree Species Richness", y="Number of Plots",x="Species Richness")+theme(plot.title = element_text(hjust = 0.5))
+
+
+#Shannons index (H)
+
+#divided by unitcd
+ggplot(shantest, aes(x= Shannon))+geom_histogram(binwidth = .05)+ labs(title = "Distribution of Plot Tree Shannon's Index", y="Number of Plots",x="Shannon's Index")+facet_grid(UNITCD~.,labeller = labeller(UNITCD=supp.labs))+theme(plot.title = element_text(hjust = 0.5))
+supp.labs <- c("Delta", "North","Central","South","Southwest")
+names(supp.labs)<- c("1","2","3","4","5")
+
+#overall
+ggplot(shantest, aes(x= Shannon))+geom_histogram(binwidth = .05)+ labs(title = "Distribution of Plot Tree Shannon's Index", y="Number of Plots",x="Shannon's Index")+theme(plot.title = element_text(hjust = 0.5))
 
 
 # #PAST work
